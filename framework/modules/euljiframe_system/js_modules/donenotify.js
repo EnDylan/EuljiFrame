5db4472b1af0042b54e5417b10cc5399cd111288
Dylan Kok <EnDylan@users.noreply.github.com>
2017-05-26 23:07:32 +0800

    MIME Error Hotfix

---
+++ framework/modules/euljiframe_system/js_modules/donenotify.js
@@ -0,0 +1,7 @@
+setTimeout(function () {
+console.log("");
+console.log("EuljiFrame fully loaded.");
+}, 1500);
+setTimeout(function () {
+console.log("Done. Check console for extension errors.");
+}, 3000);
