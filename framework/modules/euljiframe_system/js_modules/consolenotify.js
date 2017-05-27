4121559f6d7f0fef0ed80cd0925786069acdc95d
Dylan Kok <EnDylan@users.noreply.github.com>
2017-05-27 19:58:31 +0800

    Fix Version Error in Console

--- framework/modules/euljiframe_system/js_modules/consolenotify.js
+++ framework/modules/euljiframe_system/js_modules/consolenotify.js
@@ -7,5 +7,5 @@
     on this file and your main HTML does not match
 */
 
-console.log("\nEuljiFrame Release Info:\n        Version: Release Nowon 1.1.0 (Nowon/노원)\n        Date: 26 May 2017\n        Website: https://endylan.eulj.io/a/EuljifyHTML/\n\n    Copyright (c) 2017, EuljifyHTML.\n        Product License: GNU Affero General Public License v3.0.\n\n    EuljiFrame is based on HTML5Up.net Stellar:\n        License: Creative Commons Attribution 3.0 License\n.")
+console.log("\nEuljiFrame Release Info:\n        Version: Release Nowon 1.1.1 (Nowon/노원)\n        Date: 26 May 2017\n        Website: https://endylan.eulj.io/a/EuljifyHTML/\n\n    Copyright (c) 2017, EuljifyHTML.\n        Product License: GNU Affero General Public License v3.0.\n\n    EuljiFrame is based on HTML5Up.net Stellar:\n        License: Creative Commons Attribution 3.0 License\n.")
 console.log("Firing jsModuleHandler")
